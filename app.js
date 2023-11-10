64b8c4947d9fc8f3738e38518416a7f070140ce1
Zach <zacharyali12@gmail.com>
2023-11-10 15:32:27 -0400

    Very strong version of the game but not quite there yet

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
-function ChoosingAButton() {
-    const elements = $("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button");
-    const randomIndex = Math.floor(Math.random() * elements.length);
-    const randomElement = elements.eq(randomIndex);
-    return randomElement;
-}
-
-function ButtonFlash(element) {
-    element.animate({ opacity: 0.4 }, 350);
-    element.animate({ opacity: 1 }, 250);
-}
-
-$("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button, .Level, .Score").hide();
 
 $(document).ready(function () {
+    let score = 0;
+    let level = 1;
+    let sequence = [];
+    let userClicks = 0;
+
+    function ChoosingAButton() {
+        const elements = $("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button");
+        const randomIndex = Math.floor(Math.random() * elements.length);
+        const randomElement = elements.eq(randomIndex);
+        return randomElement;
+    }
+
+    function ButtonFlash(element) {
+        element.animate({ opacity: 0.4 }, 350);
+        element.animate({ opacity: 1 }, 250);
+    }
+
+    function playGame() {
+        let sequenceIndex = 0;
+
+        function flashNext() {
+            if (sequenceIndex < sequence.length) {
+                ButtonFlash(sequence[sequenceIndex]);
+                sequenceIndex++;
+                setTimeout(flashNext, 1000);
+            } else {
+
+                $(".Button").click(function () {
+                    const clickedButton = $(this);
+                    ButtonFlash(clickedButton);
+
+                    if (clickedButton[0] !== sequence[userClicks][0]) {
+                        alert("Game over! Score: " + score);
+
+                        score = 0;
+                        level = 1;
+                        sequence = [];
+                        userClicks = 0;
+                        $(".Score").text("Score: " + score);
+                        $(".Level").text("Level: " + level);
+                        return;
+                    }
+                    userClicks++;
+
+                    if (userClicks === sequence.length) {
+                        score++;
+                        level++;
+                        userClicks = 0;
+                        $(".Score").text("Score: " + score);
+                        $(".Level").text("Level: " + level);
+                        sequenceIndex = 0;
+                        setTimeout(playNextLevel, 1000);
+                    }
+                });
+            }
+        }
+
+        function playNextLevel() {
+            sequence.push(ChoosingAButton());
+            flashNext();
+        }
+
+        playNextLevel();
+    }
+
+    $("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button, .Level, .Score").hide();
+
     $("button.Start_Button").click(function () {
         $("button#Red_Button, button#Blue_Button, button#Green_Button, button#Yellow_Button, .Level, .Score").toggle();
         $("button.Start_Button, .Start_Button, .Start, .Title").hide();
-    });
-
-    $("button.Start_Button").click(function () {
-        const randomElement = ChoosingAButton();
-        ButtonFlash(randomElement);
-
-        if ($(randomElement).click(function () {
-            alert("hello");
-        }));
-        else {
-            alert("bye")
-        }
+        playGame();
     });
 });
 
