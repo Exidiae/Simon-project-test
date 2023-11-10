f36821a35cf56a3661381fa7a40387c4f7ec44e4
Zach <zacharyali12@gmail.com>
2023-11-10 09:03:51 -0400

    opacity working for 1 for now lol

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// const this_is_javascript = alert("hello")
+
+const elements = $(".Red_Button, .Blue_Button, .Green_Button, .Yellow_Button");
+
+$(".Red_Button, .Blue_Button, .Green_Button, .Yellow_Button, .Level, .Score").hide();
+
+$(document).ready(function () {
+    $("button.Start_Button").click(function () {
+        $("button.Red_Button, button.Blue_Button, button.Green_Button, button.Yellow_Button, .Level, .Score").toggle();
+        $("button.Start_Button, .Start_Button, .Start, .Title").hide()
+    });
+});
+
+$("button.Start_Button").click(function () {
+    const randomIndex = Math.floor(Math.random() * elements.length);
+    const randomElement = elements.eq(randomIndex);
+    randomElement.animate({ opacity: 0.4 }, 250);
+});
