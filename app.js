2f965bfaaf3d2fe340dedc1533a099a5cd739506
Zach <zacharyali12@gmail.com>
2023-11-11 12:04:23 -0400

    Ok so it has sequence and record scores only once

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-    let score = 0;
     let level = 1;
     let sequence = [];
-    let userClicks = 0;
+    let userClicks = [];
+    let inProgress = false;
+    let Score = 0
+
+    $("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button, .Level, .Score").hide();
 
     function ChoosingAButton() {
         const elements = $("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button");
@@ -16,58 +19,42 @@ $(document).ready(function () {
         element.animate({ opacity: 1 }, 250);
     }
 
-    //----------------------------------------------------------------------------------------
-    function playGame() {
-        let sequenceIndex = 0;
-
-        function flashNext() {
-            if (sequenceIndex < sequence.length) {
-                ButtonFlash(sequence[sequenceIndex]);
-                sequenceIndex++;
-                setTimeout(flashNext, 1000);
-            } else {
-                userClicks = 0;
-                $(".Button").off('click').on('click', function () {
-                    const clickedButton = $(this);
-                    ButtonFlash(clickedButton);
-                    if (clickedButton[0] === sequence[userClicks][0]) {
-                        userClicks++;
-                        if (userClicks === sequence.length) {
-                            score++;
-                            $(".Score").text("Score: " + score);
-                            level++;
-                            $(".Level").text("Level: " + level);
-                            setTimeout(playNextLevel, 1000);
-                        }
-                    } else {
-                        alert("Game over! Score: " + score);
-                        score = 0;
-                        level = 0;
-                        sequence = [];
-                        $(".Score").text("Score: " + score);
-                        $(".Level").text("Level: " + level);
-                        setTimeout(playNextLevel, 1000);
-                    }
-                });
-            }
-        }
-
-        function playNextLevel() {
-            sequence = [];
-            for (let i = 0; i < level; i++) {
-                sequence.push(ChoosingAButton());
-            }
-            flashNext();
-        }
-
-        playNextLevel();
+    function startNewLevel() {
+        const randomElement = ChoosingAButton();
+        sequence.push(randomElement);
+        ButtonFlash(randomElement);
+        $(".Level").text("Level " + level);
+        userClicks = [];
+        inProgress = true;
     }
 
-    $("#Red_Button, #Blue_Button, #Green_Button, #Yellow_Button, .Level, .Score").hide();
+    function increaseScore() {
+        Score++;
+        $("#scoreValue").text(Score);
+        console.log(Score);
+    }
+
+    function addNextButtonToSequence() {
+        const randomElement = ChoosingAButton();
+        ButtonFlash(randomElement);
+        sequence.push(randomElement);
+    }
+    //-------------------------------------------------------------
+
+    //-------------------------------------------------------------
 
     $("button.Start_Button").click(function () {
         $("button#Red_Button, button#Blue_Button, button#Green_Button, button#Yellow_Button, .Level, .Score").toggle();
         $("button.Start_Button, .Start_Button, .Start, .Title").hide();
-        playGame();
+
+        const randomElement = ChoosingAButton();
+        ButtonFlash(randomElement);
+
+        randomElement.click(function () {
+            increaseScore();
+            addNextButtonToSequence();
+        });
+
     });
 });
+
